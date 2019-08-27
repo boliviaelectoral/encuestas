ca7b52e8ab7181c6eba10e8d3d8c8ea8f588d5ef
rafa_lopezv <lopezvalverde.rafael@gmail.com>
2019-08-27 19:17:54 -0400

    grafico de ponderaciones
    last push

--- tablas.R
+++ tablas.R
@@ -11,7 +11,7 @@ extrafont::font_import()
 bd <- gs_title("Monitoreo de encuestas, drive privado")
 
 # lista de hojas
-Monitoreo de encuestas, drive privado
+# Monitoreo de encuestas, drive privado
 
 # seleccion de hoja
 df <- gs_read(ss = bd, ws = "fuentes_encuestas")
@@ -66,6 +66,97 @@ reactable(df, filterable = TRUE, pagination = FALSE, columns = list(
  )
 ) %>% 
   htmlwidgets::saveWidget(here::here("img", "tabla.html"))
+
+
+# procesamiento de tabla para público
+encuestas <- rio::import("output_para_procesar/encuestas.xlsx") %>% 
+  select(-mes, -año)
+fichas <- readxl::read_excel("output_para_procesar/fichas.xlsx") %>% 
+  select(-año, -no, -método)
+
+df_1 <- merge(encuestas, fichas)
+
+#  cambio de nombres
+df_1$partido_o_alianza %<>% gsub("CC", "Comunidad Ciudadana", .)
+df_1$partido_o_alianza %<>% gsub("MTS", "Movimiento Tercer Sistema", .)
+df_1$partido_o_alianza %<>% gsub("MNR", "Moviemiento Nacionalista Revolucionario", .)
+df_1$partido_o_alianza %<>% gsub("PDC", "Partido Demócrata Cristino", .)
+df_1$partido_o_alianza %<>% gsub("PAN-BOL", "Partido de Acción Nacional Boliviano", .)
+df_1$partido_o_alianza %<>% gsub("FPV", "Frente para la Victoria", .)
+df_1$partido_o_alianza %<>% gsub("21F", "Bolivia dijo No", .)
+df_1$partido_o_alianza %<>% gsub("UCS", "Unión Cívica Solidaridad", .)
+
+df_1$candidato_a_la_presidencia %<>% gsub("Morales", "Evo Morales", .)
+df_1$candidato_a_la_presidencia %<>% gsub("Mesa", "Carlos Mesa", .)
+df_1$candidato_a_la_presidencia %<>% gsub("Patzi", "Félix Patzi", .)
+df_1$candidato_a_la_presidencia %<>% gsub("Lema", "Virginio Lema", .)
+df_1$candidato_a_la_presidencia %<>% gsub("Quiroga", "Jorge Quiroga", .)
+df_1$candidato_a_la_presidencia %<>% gsub("Paz Zamora", "Jaime Paz Zamora", .)
+df_1$candidato_a_la_presidencia %<>% gsub("Nina", "Ruth Nina", .)
+df_1$candidato_a_la_presidencia %<>% gsub("Rodríguez", "Israel Rodríguez", .)
+df_1$candidato_a_la_presidencia %<>% gsub("Albarracín", "Waldo Albarracín", .)
+df_1$candidato_a_la_presidencia %<>% gsub("Revilla", "Luis Revilla", .)
+df_1$candidato_a_la_presidencia %<>% gsub("Costas", "Rubén Costas", .)
+df_1$candidato_a_la_presidencia %<>% gsub("Doria Medina", "Samuel Doria Medina", .)
+df_1$candidato_a_la_presidencia %<>% gsub("Ortiz", "Oscar Ortiz", .)
+df_1$candidato_a_la_presidencia %<>% gsub("Cárdenas", "Victor Hugo Cárdenas", .)
+df_1$candidato_a_la_presidencia %<>% gsub("Voto secreto", "El voto es secreto", .)
+
+
+
+df_1$encuestadora %<>% gsub("_", " ", .)
+df_1$encuestadora %<>% str_to_title(.)
+df_1$encuestadora %<>% gsub("Mercados Y Muestras", "Mercados y Muestras", .)
+
+
+df_1$medio %<>% gsub("pg7", "Página 7", .) 
+df_1$medio %<>% gsub("pat", "PAT", .) 
+df_1$medio %<>% gsub("rtp", "RTP", .) 
+df_1$medio %<>% gsub("atb", "ATB", .) 
+df_1$medio %<>% gsub("la_razon", "La Razón", .) 
+
+# cambiar nombres de columnas, horizontalizar, correccion de valores
+df_1 %<>% 
+  select(-partido_o_alianza, -codigo_encuesta) %>% 
+  spread(candidato_a_la_presidencia, valor) %>% 
+  select(encuestadora, `Evo Morales`, `Carlos Mesa`, `Oscar Ortiz`, `Félix Patzi`, `Victor Hugo Cárdenas`, `Virginio Lema`,
+         `Israel Rodríguez`, `Ruth Nina`, `Jaime Paz Zamora`, `Jorge Quiroga`, `Luis Revilla`, `Rubén Costas`, `Samuel Doria Medina`, 
+         `Waldo Albarracín`, Otros, Indecisos, Blanco, Nulo, `Blanco/Nulo`, `El voto es secreto`, Ninguno, `No contesta`, 
+         `NS/NR`, medio, alcance_muestra, margen_error, confianza, `ciudades_capitales (+EA)`, ciudades_intermedias, area_rural, 
+         fecha_inicio_encuesta, fecha_conclusion_encuesta) %>% 
+  rename(
+    Encuestadora = encuestadora,
+    `Medio de comunicación de difusión` = medio,
+    `Márgen de error` = margen_error,
+    Confianza = confianza,
+    `Tamaño de la muestra` = alcance_muestra,
+    `Ciudades capitales + El Alto` = `ciudades_capitales (+EA)`,
+    `Ciudades intermedias` = ciudades_intermedias,
+    `Localidades rurales` = area_rural,
+    `Fecha de inicio de la encuesta` = fecha_inicio_encuesta,
+    `Fecha de conclusión de la encuesta` = fecha_conclusion_encuesta
+  ) %>% 
+  mutate(
+    `Márgen de error` = `Márgen de error` * 100,
+    Confianza = Confianza * 100,
+    `Fecha de inicio de la encuesta` = janitor::excel_numeric_to_date(`Fecha de inicio de la encuesta`),
+    `Fecha de conclusión de la encuesta` = janitor::excel_numeric_to_date(`Fecha de conclusión de la encuesta`)
+  ) %>% 
+  arrange(`Fecha de conclusión de la encuesta`) 
+
+# empujar a drive
+df_1 %>% 
+  rio::export("output_para_referencia/base_datos_drive.xlsx")
+
+
+
+
+
+  
+
+
+
+
 
 
 
---
+++ tendencia.R
@@ -0,0 +1,78 @@
+#-----------------------
+# para datos ponderados
+#-----------------------
+# extraer archivo
+bd <- gs_title("Monitoreo de encuestas, drive privado")
+
+# extaer hoja
+df <- gs_read(ss = bd, ws = "Encuestas ponderadas ")
+
+# manipulaciones para el gráfico
+df %<>% 
+  janitor::clean_names() %>% 
+  mutate(
+    num = 1:nrow(.),
+    fecha = paste0(mes, " ", ano)
+  )
+
+evo <- df %>% filter(candidato_a_la_presidencia == "Morales")
+mesa <- df %>% filter(candidato_a_la_presidencia == "Mesa")
+ortiz <- df %>% filter(candidato_a_la_presidencia == "Ortiz")
+
+
+hc3 <- highchart() %>% 
+  hc_add_series(evo, type = "line", color = highcharter::hex_to_rgba("blue", alpha = 0.2), 
+                hcaes(x = fecha, y = valor),
+                tooltip = list(pointFormat = paste("<b>Evo Morales:<b> {point.valor} %<br>"), headerFormat = ""),
+                name = "Evo Morales", id = "morales") %>% 
+  hc_add_series(fit_evo, type = "line", color = "blue", 
+                hcaes(x = no, y = .fitted),
+                tooltip = list(pointFormat = "Línea de tendencia", headerFormat = ""),
+                name = "Fit", linkedTo = "morales") %>% 
+  hc_add_series(mesa, type = "line", color = highcharter::hex_to_rgba("orange", alpha = 0.2), 
+                hcaes(x = fecha, y = valor),
+                tooltip = list(pointFormat = paste("<b>Carlos Mesa:<b> {point.valor} %<br>"), headerFormat = ""),
+                name = "Carlos Mesa", id = "mesa") %>%
+  hc_add_series(fit_mesa, type = "line", color = "orange", 
+                hcaes(x = no, y = .fitted),
+                tooltip = list(pointFormat = "Línea de tendencia", headerFormat = ""),
+                name = "Fit", linkedTo = "mesa") %>% 
+  hc_add_series(ortiz, type = "line", color = highcharter::hex_to_rgba("red", alpha = 0.2), 
+                hcaes(x = fecha, y = valor),
+                tooltip = list(pointFormat = paste("<b>Oscar Ortiz:<b> {point.valor} %<br>"), headerFormat = ""),
+                name = "Oscar Ortiz", id = "ortiz") %>%
+  hc_add_series(ort_fecha, type = "line", color = "red", 
+                hcaes(x = fecha, y = .fitted),
+                tooltip = list(pointFormat = "Línea de tendencia", headerFormat = ""),
+                name = "Fit", linkedTo = "ortiz") %>% 
+  hc_xAxis(categories = evo$fecha,
+           tickmarkPlacement = "on",
+           title = list(enabled = T)) %>% 
+  hc_plotOptions(line = list(
+    lineWidth = 4,
+    connectNulls = F,
+    animation = list(
+      duration = 5000 
+    ),
+    marker = list(
+      lineWidth = 300,
+      lineColor = "#ffffff",
+      enabled = F
+    ),
+    dataLabels = list(
+      enabled = F,
+      format = "{point.valor:.0f} %"
+    ))
+  ) %>% 
+  hc_add_theme(hc_theme_elementary()) %>% 
+  hc_credits(enabled = TRUE, text = "Bolivia Electoral", href = "http://www.boliviaelectoral.com/") %>% 
+  hc_exporting(
+    enabled = TRUE
+  ) %>% 
+  hc_yAxis(title = list(text = "Intención de voto")) %>% 
+  hc_title(text = "Ponderaciópn de encuestas y ajuste de tendencia") %>% 
+  hc_subtitle(text = "Elecciones generales Bolivia 2019") 
+  
+htmlwidgets::saveWidget(hc3, here::here("img", "tendencia.html"))  
+   
+  
